6163b4e2eff656bb691ec511e186d10a04bb43c7
fatima <fatimarettea@yahoo.com>
2024-11-20 09:42:57 -0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import {Routes, Route} from 'react-router-dom'
 
 
 
@@ -7,6 +7,11 @@ export default function App() {
   return (
     <main>
       <h1>Fitness</h1>
+
+      <Routes>
+        <Route path ="/" element={<HomePage />}/>
+        
+      </Routes>
     </main>
   )
 }
